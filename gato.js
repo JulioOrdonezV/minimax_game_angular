ffd5d4ba2f8be11e22cca15809ef8257481cf128
Julio Ordonez Viales <julio7k@gmail.com>
2015-10-21 00:52:51 -0600

    Implement minimax algorithm

--- gato.js
+++ gato.js
@@ -89,7 +89,6 @@ var gato = angular.module('gato',[]);
 			//displays mark
 
 			$scope.checkCell($scope.playerIcon.icon, cell);
-			$scope.turnCount++;
 
 
 			if($scope.checkWinner() != '') $scope.showWinner($scope.checkWinner()) ;
@@ -103,12 +102,11 @@ var gato = angular.module('gato',[]);
 						$scope.randomMove();
 					};
 					if($scope.activeLevel == $scope.myLevels[1]){
-						$scope.calculateMove();
-					};
-					$scope.turnCount++;
+						var Guess = $scope.calculateMove();
+						$scope.checkCell($scope.computerIcon.icon, Guess.move);
+					}
 				}
 			}
-
 			if($scope.checkWinner() != '') $scope.showWinner($scope.checkWinner()) ;
 		};
 
@@ -121,6 +119,7 @@ var gato = angular.module('gato',[]);
 		$scope.checkCell = function(icon, cell){
 			cell.value = icon;
 			cell.isDisabled = true;
+			$scope.turnCount++;
 		};
 
 		$scope.randomMove = function(){
@@ -133,7 +132,49 @@ var gato = angular.module('gato',[]);
 		};
 
 		$scope.calculateMove = function(){
+			var myBest = {score: "", move:""};
+			var reply;
 
+			if($scope.turnCount == 9 || $scope.checkWinner() != ''){
+				if($scope.checkWinner() == $scope.playerIcon.icon)
+					myBest.score = -1;
+				else if($scope.checkWinner() == $scope.computerIcon.icon)
+				  myBest.score = 1;
+				else {
+					myBest.score = 0;
+				}
+				return myBest;
+			}
+
+			if(($scope.turnCount % 2) == 0){ //user turn make a function to check user turn, computer turn
+        myBest.score = 2; //   at least picks one move, even if it loses
+    	} else {  //computer turn
+        myBest.score = -2;
+    	}
+
+			for(var row = 0 ; row < $scope.myCells.length ; row++){
+				for(var col = 0; col < $scope.myCells[0].length; col++){
+					if($scope.myCells[row][col].value == ''){
+						if(($scope.turnCount % 2) == 0){ //user turn
+							$scope.checkCell($scope.playerIcon.icon, $scope.myCells[row][col]);
+						} else {
+							$scope.checkCell($scope.computerIcon.icon, $scope.myCells[row][col]);
+						}
+						reply = $scope.calculateMove();
+						$scope.myCells[row][col].value = '';
+						$scope.myCells[row][col].isDisabled = false;
+						$scope.turnCount = $scope.turnCount - 1;
+						if ((($scope.turnCount % 2 != 0) &&
+		            (reply.score > myBest.score)) || //computer chooses the highest score
+		            (($scope.turnCount % 2 == 0) &&
+		            (reply.score < myBest.score))){ //humna chooses the lowest score
+									myBest.move = $scope.myCells[row][col];
+									myBest.score = reply.score;
+								}
+					}
+				}
+			}
+			return myBest;
 		};
 
 		$scope.checkWinner = function(){
@@ -181,11 +222,15 @@ var gato = angular.module('gato',[]);
 		    return Math.floor(Math.random() * (max - min + 1)) + min;
 		}
 
-		function setGameEnabled(status){
+		function setGameEnabled(game_enabled){
 			for(var row = 0 ; row< $scope.myCells.length ; row++){
 				for(var col = 0; col < $scope.myCells[0].length; col++){
-					$scope.myCells[row][col].isDisabled = !status;
-					if(status)$scope.myCells[row][col].value = '';
+					if(game_enabled){
+						$scope.myCells[row][col].value = '';
+						$scope.myCells[row][col].isDisabled = false;
+					} else {
+						$scope.myCells[row][col].isDisabled = true;
+					}
 				}
 			}
 		}
